229f96e5adfb9f3d546d8a1aab157b0e51031545
AlexDel <verbalab@yandex.ru>
2016-06-11 14:21:03 +0700

    further tagging

--- nlrequestprocess.py
+++ nlrequestprocess.py
@@ -1,10 +1,12 @@
 import nltk
 
-testString = 'honda accord задняя стойка'
+testString = 'задняя стойка от honda accord '
 
 tagPatterns = [
    (r'(honda)$','VENDOR'),
-   (r'([a-zA-Z0-9]+)$','MODEL')
+   (r'([a-zA-Z0-9]+)$','MODEL'),
+   (r'(от|для)$','PREP'),
+   (r'([а-яА-Я]+)$','PART_NAME'),
 ]
 
 tagger = nltk.RegexpTagger(tagPatterns)
