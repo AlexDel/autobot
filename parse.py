3507c7ad7bb5c58bc9180a3a5f8f2d3c1407dbba
AlexDel <verbalab@yandex.ru>
2016-06-12 11:56:59 +0700

    first token is taken

--- parse.py
+++ parse.py
@@ -4,16 +4,17 @@ from cssselect import GenericTranslator
 from lxml.etree import XPath
 
 def __parse_list(text, css):
-	parser = etree.HTMLParser()
-	tree = etree.parse(StringIO(text), parser)
-	xpath = XPath(GenericTranslator().css_to_xpath(css))
-	result = tree.xpath(str(xpath))
-	return result
+    parser = etree.HTMLParser()
+    tree = etree.parse(StringIO(text), parser)
+    xpath = XPath(GenericTranslator().css_to_xpath(css))
+    result = tree.xpath(str(xpath))
+    return result
 
 def parse_models_list(text):
-	result = __parse_list(text, 'div dd a')
-	models_list = {model: link for (model, link) in map(lambda x: (list(x.itertext())[0], x.get('href')), result)}
-	return models_list
+    result = __parse_list(text, 'div dd a')
+    models_list = {model.split()[0]: link for (model, link) in map(lambda x: (list(x.itertext())[0], x.get('href')), result)}
+    print(models_list)
+    return models_list
 
 def parse_complectations_list(text):
 	result = __parse_list(text, '')
