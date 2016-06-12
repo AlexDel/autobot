501c8de8bc3acf0dc9ae87a007c2b3a0f6116acc
AlexDel <verbalab@yandex.ru>
2016-06-12 13:40:23 +0700

    phrase similiarity function

--- nltkutils.py
+++ nltkutils.py
@@ -1,4 +1,5 @@
 from nltk.metrics.distance import edit_distance
+from statistics import mean
 import operator
 
 
@@ -6,3 +7,16 @@ def get_closest_word(list, word):
 		distances = {w: dist for (w, dist) in map(lambda x: (x, edit_distance(word, x.split()[0])), list)}
 		# There should be 'min' but WTF?!
 		return max(distances)
+
+def calc_phrase_distance(phrase1, phrase2):
+	tokens1 = phrase1.split()
+	tokens2 = phrase2.split()
+
+	tokens_distances = []
+	for t1 in tokens1:
+		tokens_distances.append(min(map(lambda token: edit_distance(t1, token), tokens2)))
+
+	return sum(tokens_distances)/mean(map(len,[phrase1, phrase2]))
+
+def get_closest_phrase(phrase, phrases_list):
+	return True
